8998a9720ae523fc00447bafdfa6fd933229633d
Alexey <alexey.chezganov@urfu.me>
2023-12-24 15:48:46 +0500

    Rain Invoke update 0.1
    *Добавлена система смены погоды (ясно/паспмурно+дождь)

---
+++ Assets/Scripts/RainInvocation.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainInvocation : MonoBehaviour
+{
+    public Light dirLight; // сслыка на directionLight
+    private ParticleSystem _ps; // объявление переменной типа part. sys.
+    private bool _isRain = false; 
+
+    private void Start()
+    {
+        _ps = GetComponent<ParticleSystem>(); // инициализация
+        StartCoroutine(Weather()); // запуск куротины
+    }
+
+    private void Update()
+    {
+        if (_isRain && dirLight.intensity > 0.3f)
+            LightIntensity(-1);
+        else if (!_isRain && dirLight.intensity < 1.0f)
+            LightIntensity(1);
+
+    }
+
+    private void LightIntensity(int mult)
+    {
+        dirLight.intensity += 0.1f * Time.deltaTime * mult;
+    }
+
+    IEnumerator Weather()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(60f, 120f));
+            if (_isRain) _ps.Stop();
+            else _ps.Play();
+            _isRain = !_isRain;
+        }
+    }
+}
